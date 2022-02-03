6aaac8901b508e782b6bb682d066e35059ac62b2
Prajesh Dutta <95134037+prajesh2004@users.noreply.github.com>
2022-02-03 23:00:59 +0530

    Add files via upload

---
+++ Demo game.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int c=45;
+int main()
+{
+    //int a, b, c;
+
+
+    /*cout<<"Enter the value of a:"<<endl;
+    cin>>a;
+    cout<<"Enter the value of b:"<<endl;
+    cin>>b;
+    c=a+b;
+    cout<<"The sum is C"<<c;
+    cout<<"The global C is"<<::c;*/
+    //long double e=34.4;
+    //cout<<"The value of d is"<<endl<<d<<"The value of e is"<<e;
+    /*float d=34.4F;
+    long double e=34.4L;
+    cout<<"The size of 34.4 is"<<sizeof(34.4)<<endl;
+    cout<<"The size of 34.4f is"<<sizeof(34.4f)<<endl;
+    cout<<"The size of 34.4F is"<<sizeof(34.4F)<<endl;
+    cout<<"The size of 34.4l is"<<sizeof(34.4l)<<endl;
+    cout<<"The size of 34.4L is"<<sizeof(34.4L)<<endl;*/
+    int a =45;
+    float b = 45.46;
+    cout<<"The value of a is"<<(float)a<<endl;
+    cout<<"The value of b is"<<float(a)<<endl;
+
+    cout<<"The value of b is "<<(int)b<<endl;
+    cout<<"The value of b is"<<int(b)<<endl;
+    int c = int (b);
+    cout<<"The expression  is"<<c<<a+b;
+
+    return 0;
+    
+
+
+}
